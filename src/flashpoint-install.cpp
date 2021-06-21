509b9fcabcacfcbdffaa7fd492316fc027e8ad85
Chris Heimlich <chris@pcserenity.com>
2021-06-21 02:25:34 -0400

    Updated flashpoint-install to latest from OFILb

--- src/flashpoint-install.cpp
+++ src/flashpoint-install.cpp
@@ -1,6 +1,7 @@
 #include "flashpoint-install.h"
 #include "qx-io.h"
 #include "qx-windows.h"
+#include "clifp.h"
 
 namespace FP
 {
@@ -357,24 +358,6 @@ Qx::GenericError Install::JsonServicesReader::readInto()
     else
         return parseServicesDocument(servicesDocument);
 }
-
-//===============================================================================================================
-// INSTALL::CLIFp
-//===============================================================================================================
-
-//-Class Functions--------------------------------------------------------------------------------------------
-//Public:
-QString Install::CLIFp::parametersFromStandard(QString originalAppPath, QString originalAppParams)
-{
-    if(originalAppPath == DBTable_Add_App::ENTRY_MESSAGE)
-        return MSG_ARG.arg(originalAppParams) + " -q";
-    else if(originalAppPath == DBTable_Add_App::ENTRY_EXTRAS)
-        return EXTRA_ARG.arg(originalAppParams) + " -q";
-    else
-        return APP_ARG.arg(originalAppPath) + " " + PARAM_ARG.arg(originalAppParams) + " -q";
-}
-
-QString Install::CLIFp::parametersFromStandard(QUuid titleID) { return AUTO_ARG.arg(titleID.toString(QUuid::WithoutBraces)) + " -q"; }
 
 //===============================================================================================================
 // INSTALL
--- src/flashpoint-install.h
+++ src/flashpoint-install.h
@@ -62,6 +62,7 @@ public:
         QStringList arguments;
         bool kill;
     };
+
     struct StartStop
     {
         QString path;
@@ -411,23 +412,6 @@ public:
 
     public:
         Qx::GenericError readInto();
-    };
-
-    class CLIFp
-    {
-    // Class members
-    public:
-        static inline const QString EXE_NAME = "CLIFp.exe";
-        static inline const QString APP_ARG = R"(--exe="%1")";
-        static inline const QString PARAM_ARG = R"(--param="%1")";
-        static inline const QString EXTRA_ARG = R"(--extra-"%1")";
-        static inline const QString MSG_ARG = R"(--msg="%1")";
-        static inline const QString AUTO_ARG = R"(--auto="%1")";
-
-    // Class functions
-    public:
-        static QString parametersFromStandard(QString originalAppPath, QString originalAppParams);
-        static QString parametersFromStandard(QUuid titleID);
     };
 
 //-Class Variables-----------------------------------------------------------------------------------------------
